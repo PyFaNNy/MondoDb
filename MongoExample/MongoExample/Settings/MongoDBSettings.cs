a69f60fd01927ae834afc5162a27957d99836d03
PyFaNNy <fanny.koob@gmail.com>
2022-12-13 19:59:10 +0300

    implement

---
+++ MongoExample/MongoExample/Settings/MongoDBSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MongoExample.Settings;
+
+public class MongoDBSettings
+{
+    public string ConnectionURI { get; set; }
+    public string DatabaseName { get; set; }
+    public string CollectionName { get; set; }
+}
